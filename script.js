1a2f4ffc8d1aef0dabce3bf4b5057aa13bc43f0f
Ashay <101341591+ashaydharmik@users.noreply.github.com>
2022-10-16 10:52:05 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,18 @@
+	// Sticky Navbar
+    let header = document.querySelector('header');
+    let menu = document.querySelector('#menu-icon');
+    let navbar = document.querySelector('.navbar');
+     
+     
+    window.addEventListener('scroll', () => {
+        header.classList.toggle('shadow', window.scrollY > 0);
+    });
+     
+    menu.onclick = () => {
+        navbar.classList.toggle('active');
+    }
+    window.onscroll = () => {
+        navbar.classList.remove('active');
+    }
+     
+    
