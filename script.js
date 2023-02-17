52c5cc254a94ab8b8b0716dd9b6e5c2ca2cc3ad8
Ashay <101341591+ashaydharmik@users.noreply.github.com>
2023-02-17 20:26:59 +0530

    Update script.js

--- script.js
+++ script.js
@@ -15,4 +15,49 @@
         navbar.classList.remove('active');
     }
      
+
+   var form=document.getElementById('form')
+   form.addEventListener('submit',function(event){
+    event.preventDefault()
+    alert('Submitted')
+
+    var name=document.getElementById('username').value
+    console.log(name)
+    var email=document.getElementById('mail').value
+    console.log(email)
+    var message=document.getElementById('msg').value
+    console.log(message)
+   })
+
+
+    // $(function() {
     
+    //     var $meters = $(".meter > span");
+    //     var $section = $('skills');
+    //     var $queue = $({});
+        
+    //     function loadDaBars() {
+    //                 $(".meter > span").each(function() {
+    //                     $(this)
+    //                         .data("origWidth", $(this).width())
+    //                         .width(0)
+    //                         .animate({
+    //                             width: $(this).data("origWidth")
+    //                         }, 1200);
+    //                 });
+    //     }
+        
+    //     $(document).bind('scroll', function(ev) {
+    //         var scrollOffset = $(document).scrollTop();
+    //         var containerOffset = $section.offset().top - window.innerHeight;
+    //         if (scrollOffset > containerOffset) {
+    //             loadDaBars();
+    //             // unbind event not to load scrolsl again
+    //             $(document).unbind('scroll');
+    //         }
+    //     });
+        
+    // });
+
+
+
